db24c7e68fc2f793eeae17003e1a3f0b672c9778
Kevin Mader <kevinmader@gmail.com>
2013-10-04 16:00:08 +0200

    added image reading and test data to the R integration tools

--- src/R/tiplIntegration.R
+++ src/R/tiplIntegration.R
@@ -1,2 +1,16 @@
-.jinit(classpath="/Users/mader/Dropbox/TIPL/build/TIPL_beta.jar",parameters="-Xmx4g")
-.jaddClassPath("/Users/mader/Dropbox/TIPL/build/TIPL_beta.jar")
+require("rJava")
+.jinit(classpath="/Users/mader/Dropbox/TIPL/build/TIPL.jar",parameters="-Xmx4g -Djava.awt.headless=true")
+.jaddClassPath("/Users/mader/Dropbox/TIPL/build/TIPL.jar")
+# Test Functions
+as.tipl.pf<-function(obj) .jcast(obj,new.class="tipl/formats/PureFImage$PositionFunction",check=T)
+tiplPlane<-as.tipl.pf(.jnew("tipl/tests/TestFImages$DiagonalPlaneFunction"))
+tiplLines<-as.tipl.pf(.jnew("tipl/tests/TestFImages$LinesFunction"))
+# this doesnt work :: not sure why .jcall("tipl/tests/TestFImages","tipl/formats/TImgRO","wrapIt",as.integer(10),tiplPlane)
+testWrap<-function(c.fun,size=10) J("tipl.tests.TestFImages")$wrapIt(as.integer(size),c.fun)
+planeImg<-testWrap(tiplPlane)
+lineImg<-testWrap(tiplLine)
+# this gets very unhappy with AWT ReadTImg("/Users/mader/Dropbox/TIPL/test/foam/ufilt.tif")
+ReadTImg<-function(filename) J("tipl.util.TImgTools")$ReadTImg(filename)
+
+
+
