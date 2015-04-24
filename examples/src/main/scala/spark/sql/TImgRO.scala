8a4778f4424cbcbd6353d27350f9aaad791dc240
Kevin Mader <kevinmader@gmail.com>
2015-04-24 10:09:52 +0200

    first implementation for sql, field support still doesnt work

--- examples/src/main/scala/spark/sql/TImgRO.scala
+++ examples/src/main/scala/spark/sql/TImgRO.scala
@@ -6,7 +6,8 @@ import org.apache.spark.sql.types._
 
 import tipl.formats.TImgRO
 import tipl.formats.TImg.ArrayBackedTImg
-import tipl.util.{D3float, D3int}
+import tipl.util.{ID2int, ID3int, D3float, D3int}
+
 
 /**
  * A package to bring the TIPL tools into Spark SQL to make some of the analyses much easier to
@@ -14,17 +15,37 @@ import tipl.util.{D3float, D3int}
  * Created by mader on 4/23/15.
  */
 @SQLUserDefinedType(udt = classOf[TImgROUDT])
-trait SQLTImgRO extends TImgRO {
-
+trait SQLTImgRO extends Serializable {
+  def getImg: TImgRO
 }
+
 @SQLUserDefinedType(udt = classOf[D3intUDT])
-trait SQLD3int extends D3int {
+trait SQLD3int extends ID3int {
 
 }
+case class SQLD3I(x: Int, y: Int, z: Int) extends SQLD3int {
+  override def gz(): Int = z
 
-class TImgROUDT extends UserDefinedType[TImgRO] {
+  override def setPos(ix: Int, iy: Int, iz: Int): ID3int = SQLD3I(ix,iy,iz)
+
+  override def gx(): Int = x
+
+  override def setPos(ix: Int, iy: Int): ID2int = SQLD3I(ix,iy,z)
+
+  override def gy(): Int = y
+}
+
+object CoreOps {
+  def makeImg(img: TImgRO) = new SQLTImgRO {
+    override def getImg: TImgRO = img
+  }
+  def makeD3int(pos: ID3int) = SQLD3I(pos.gx,pos.gy,pos.gz)
+}
+
+class TImgROUDT extends UserDefinedType[SQLTImgRO] {
   val d3iType = new D3intUDT().sqlType
   val d3fType = new D3floatUDT().sqlType
+
   override def sqlType: DataType = StructType(
     Seq(
       StructField("pos",d3iType,nullable=false),
@@ -37,7 +58,8 @@ class TImgROUDT extends UserDefinedType[TImgRO] {
   override def serialize(obj: Any): Any = {
     val row = new GenericMutableRow(4)
     obj match {
-      case pData: TImgRO =>
+      case wrapData: SQLTImgRO =>
+        val pData = wrapData.getImg
         val pos = pData.getPos
         row.update(0,pData.getDim)
         row.update(1,pData.getPos)
@@ -49,10 +71,10 @@ class TImgROUDT extends UserDefinedType[TImgRO] {
     row
   }
 
-  override def userClass: Class[TImgRO] = classOf[TImgRO]
+  override def userClass: Class[SQLTImgRO] = classOf[SQLTImgRO]
 
-  override def deserialize(datum: Any): TImgRO =  datum match {
-    case v: TImgRO =>
+  override def deserialize(datum: Any): SQLTImgRO =  datum match {
+    case v: SQLTImgRO =>
       System.err.println("Something strange happened, or was never serialized")
       v
     case r: Row =>
@@ -60,12 +82,16 @@ class TImgROUDT extends UserDefinedType[TImgRO] {
       val dim = r.getAs[D3int](0)
       val pos = r.getAs[D3int](1)
       val elSize = r.getAs[D3float](2)
-      ArrayBackedTImg.CreateFromSerializedData(dim,pos,elSize,r.getAs[Array[Byte]](3))
+      CoreOps.makeImg(
+        //ArrayBackedTImg.CreateFromSerializedData(dim,pos,elSize,r.getAs[Array[Byte]](3))
+        r.getAs[TImgRO](3)
+      )
   }
 }
 
 
-class D3intUDT extends UserDefinedType[D3int]  {
+class D3intUDT extends UserDefinedType[SQLD3int]  {
+
   override def sqlType: DataType = StructType(
     Seq(
       StructField("x",IntegerType,nullable=false),
@@ -77,25 +103,25 @@ class D3intUDT extends UserDefinedType[D3int]  {
   override def serialize(obj: Any): Row = {
     val row = new GenericMutableRow(3)
     obj match {
-      case pData: D3int =>
-        row.update(0,pData.x)
-        row.update(1,pData.y)
-        row.update(2,pData.z)
+      case pData: ID3int =>
+        row.update(0,pData.gx)
+        row.update(1,pData.gy)
+        row.update(2,pData.gz)
       case _ =>
         throw new RuntimeException("The given object:"+obj+" cannot be serialized by "+this)
     }
     row
   }
 
-  override def userClass: Class[D3int] = classOf[D3int]
+  override def userClass: Class[SQLD3int] = classOf[SQLD3int]
 
-  override def deserialize(datum: Any): D3int = datum match {
-    case v: D3int =>
+  override def deserialize(datum: Any): SQLD3int = datum match {
+    case v: ID3int =>
       System.err.println("Something strange happened, or was never serialized")
-      v
+      CoreOps.makeD3int(v)
     case r: Row =>
       require(r.length==3,"Wrong row-length given "+r.length+" instead of 3")
-      new D3int(
+      SQLD3I(
         r.getInt(0),
         r.getInt(1),
         r.getInt(2)
--- examples/src/test/scala/tests/SQLTests.scala
+++ examples/src/test/scala/tests/SQLTests.scala
@@ -4,8 +4,8 @@ package tests
 import fourquant.utils.SilenceLogs
 import org.apache.spark.sql.SQLContext
 import org.scalatest.{FunSuite, Matchers}
-import tests.SQLTools.NamedTImgRO
-import tipl.formats.TImgRO
+import spark.sql.{SQLD3int, SQLD3I, CoreOps, SQLTImgRO}
+import tests.SQLTools.{NamedPosition, NamedTImgRO}
 import tipl.spark.SparkGlobal
 import tipl.tests.TestPosFunctions
 import tipl.util.TImgTools
@@ -18,7 +18,21 @@ class SQLTests extends FunSuite with Matchers with SilenceLogs with Serializable
   test("Binary Type") {
 
   }
-  test("Basic SQL Test") {
+  test("Basic SQL Position Test") {
+    val ntir = sc.parallelize(1 to 10).map{
+      k => NamedPosition("k"+k,SQLD3I(k,k+1,k+2))
+    }
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+    ntir.toDF.registerTempTable("Positions")
+    val t = sqlContext.sql("SELECT name,pos from Positions")
+    t.count shouldBe 10
+    println(t.collect.mkString("\n"))
+    t.first.getString(0) shouldBe "k1"
+    val t2 = sqlContext.sql("SELECT name,pos.x,pos.y from Positions")
+    println(t2.collect.mkString("\n"))
+  }
+  test("Basic SQL Image Test") {
     val ntir = sc.parallelize(1 to 10).map{
       k => NamedTImgRO("k"+k,SQLTools.makeTestImage(k))
     }
@@ -27,14 +41,30 @@ class SQLTests extends FunSuite with Matchers with SilenceLogs with Serializable
     ntir.toDF.registerTempTable("Images")
     val t = sqlContext.sql("SELECT name,img from Images")
     t.count shouldBe 10
-    t.first.getString(0) shouldBe "1"
+    println(t.collect.mkString("\n"))
+    t.first.getString(0) shouldBe "k1"
   }
+
+  test("Field SQL Tests") {
+    val ntir = sc.parallelize(1 to 10).map{
+      k => NamedTImgRO("k"+k,SQLTools.makeTestImage(k))
+    }
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
+    ntir.toDF.registerTempTable("Images")
+    val t = sqlContext.sql("SELECT name,img.pos,img.dim,img from Images")
+    t.count shouldBe 10
+    println(t.collect.mkString("\n"))
+    t.first.getString(0) shouldBe "k1"
+  }
+
+
 }
 
 object SQLTools extends Serializable {
-  case class NamedTImgRO(name: String, img: TImgRO)
-
-
-  def makeTestImage(size: Int): TImgRO = TestPosFunctions.wrapItAs(size, new TestPosFunctions
-  .LinesFunction, TImgTools.IMAGETYPE_FLOAT)
+  case class NamedTImgRO(name: String, img: SQLTImgRO)
+  case class NamedPosition(name: String, pos: SQLD3int)
+  def makeTestImage(size: Int) = CoreOps.makeImg(
+    TestPosFunctions.wrapItAs(size, new TestPosFunctions.LinesFunction, TImgTools.IMAGETYPE_FLOAT)
+  )
 }
