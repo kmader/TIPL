a9fb9fae08180dfb201f8300dbe1545656588e88
Kevin Mader <kmader@magicleap.com>
2019-11-20 17:45:13 +0100

    removing spark-ij and spark-imageio

--- spark-ij/src/main/scala/org/apache/spark/fourquant/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The newer version of Spark made the User-defined Types obsolete from the previous version and requires that all
- * such types all under the org.apache.spark namespace. The objects have been appropriately moved into this directory
- * Created by mader on 6/20/16.
- */
-package org.apache.spark.fourquant;
